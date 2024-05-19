2a1b98fe629f133e5efecdf032fdea58fe124d61
OppaiFun <moussaadamou898@gmail.com>
2024-05-19 13:29:41 +0200

    substr c'est dans la poche

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: madamou <madamou@contact.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 14:30:23 by madamou           #+#    #+#             */
-/*   Updated: 2024/05/18 23:28:22 by madamou          ###   ########.fr       */
+/*   Updated: 2024/05/19 00:13:26 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,39 +27,33 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		while (buff->next != NULL)
 			buff = buff->next;
 		buff->next = new;
-		new->next = NULL;
 	}
 }
 
-// static t_list	*ft_lstnew1(void *content)
-// {
-// 	t_list	*new;
+/*static t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
 
-// 	new = malloc(sizeof(t_list) * 1);
-// 	if (!new)
-// 		return (NULL);
-// 	new->content = content;
-// 	new->next = NULL;
-// 	return (new);
-// }
+	new = malloc(sizeof(t_list) * 1);
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}*/
 
-// int	main(void)
-// {
-
-// 	t_list *lst = NULL;
-// 	t_list *elem1 = ft_lstnew1("Elem1");
-// 	t_list *elem2 = ft_lstnew1("Elem2");
-// 	t_list *elem3 = ft_lstnew1("Elem3");
-
-// 	ft_lstadd_back(&lst, elem1);
-// 	ft_lstadd_back(&lst, elem2);
-// 	ft_lstadd_back(&lst, elem3);
-
-// 	printf("Liste après l'ajout des éléments :\n");
-// 	while (lst != NULL)
-// 	{
-// 		printf("%s\n", (char *)lst->content);
-// 		lst = lst->next;
-// 	}
-// 	return (0);
-// }
+/*int	main(void)
+{
+	t_list * l =  NULL; t_list * l2 =  NULL;
+	ft_lstadd_back(&l, ft_lstnew((void*)1));
+	ft_lstadd_back(&l, ft_lstnew((void*)2));
+	ft_lstadd_back(&l2, ft_lstnew((void*)3));
+	ft_lstadd_back(&l2, ft_lstnew((void*)4));
+	ft_lstadd_back(&l, l2);
+	if (((t_list *)((t_list *)(((t_list *)(l->next))->next))->next)->content
+		== (void*)4)
+		printf("yes\n");
+	else
+		printf("no\n");
+ 	return (0);
+}*/
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: madamou <madamou@contact.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:47:30 by madamou           #+#    #+#             */
-/*   Updated: 2024/05/18 16:50:06 by madamou          ###   ########.fr       */
+/*   Updated: 2024/05/19 13:28:43 by madamou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	check = (unsigned long long)len;
 	if (!s)
 		return (NULL);
-	if (len >= ft_strlen(s) && start < ft_strlen(s))
+	if (len >= ft_strlen(s) && start <= ft_strlen(s))
 		str = malloc(sizeof(char) * ((ft_strlen(s) - start) + 1));
 	else if (start > ft_strlen(s))
 		str = malloc(sizeof(char) * 1);
+	else if (start + len > ft_strlen(s))
+		str = malloc(sizeof(char) * (ft_strlen(s) - start + 1));
 	else
 		str = malloc(sizeof(char) * (len + 1));
 	if (!str)
@@ -39,12 +41,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (str);
 }
 
-// int main(void)
-// {
-// 	char * str = strdup("1");
-// 	char * s = ft_substr(str, 42, 42000000);
-// 	if (!strcmp(s, ""))
-// 		printf("yes\n");
-// 	else
-// 		printf("no\n");
-// }
+/*int main(void)
+{
+ 	char * s = ft_substr("hola", 2, 3);
+ 	if (!strcmp(s, ""))
+ 		printf("yes\n");
+ 	else
+ 		printf("no\n");
+}*/
